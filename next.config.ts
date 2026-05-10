d3c4d983d865d4481c41d2c3ffc17eb94e969dfc
jawwadfirdousi <10913083+jawwadfirdousi@users.noreply.github.com>
2026-05-10 17:37:26 +0200

    Update Next.js configuration and add GitHub Actions workflow for deployment
    - Changed output setting in next.config.ts from "standalone" to "export" and added basePath configuration.
    - Updated import path in next-env.d.ts for routes type definitions.
    - Added a new GitHub Actions workflow for deploying the app to GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
